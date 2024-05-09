a4628a0a66480a3e0713808e86fd1ae747a24069
Viola <violetta.artiukhova@skey.team>
2024-05-09 13:12:34 +0200

    new project next food

---
+++ app/meals/page.js
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import styles from './MealsPage.module.css'
+import MealsGrid from '@/components/meals/MealsGrid'
+import { getMeals } from '@/lib/meals'
+
+export default async function MealsPage() {
+    const meals = await getMeals()
+
+    return (
+        <>
+            <header className={styles.header}>
+                <h1>
+                    Delicious meals, created{' '}
+                    <span className={styles.highlight}>by you</span>
+                </h1>
+                <p>
+                    Choose your favorite recipe and cook it yourself. It is easy
+                    and fun!
+                </p>
+                <p className={styles.cta}>
+                    <Link href="/meals/share"> Share Your Favorite Recipe</Link>
+                </p>
+            </header>
+            <main className={styles.main}>
+                <MealsGrid meals={meals} />
+            </main>
+        </>
+    )
+}
